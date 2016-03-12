cb4c0881f3f65d9e55e8deaaca4cb80034d983db
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2016-03-12 16:42:28 -0500

    Added modernizr for browserify for progressive enhancement purposes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var $ = require('jquery'),
 	foo = require('./test');
 
+// Modernizr tests and the trigger to run them
+require('browsernizr/test/css/flexbox');
+require('browsernizr');
+
+
+
 console.log(foo.multiply(5));
 console.log(foo.total + ' is the total!');
 
