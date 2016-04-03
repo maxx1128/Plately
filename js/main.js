afb46f5661c98c315c1eb4a71c12e6d8c9eaed2c
Maxwell Antonucci <maxwellantonucci@Maxwell-Antonuccis-MacBook-Pro.local>
2016-04-03 11:19:37 -0400

    Removed unneeded components and tightened overall code

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
-var $ = require('jquery'),
-	nav = require('./components/nav'),
-	content = require('./components/content'),
-	callout = require('./components/callout');
+// Major dependencies
+var $ = require('jquery');
 
 // Modernizr tests and the trigger to run them
 require('browsernizr/test/css/flexbox');
 require('browsernizr');
-
-nav.basic();
-nav.side();
-
-content.dropdown();
-
-callout.modal("#modal-1");
